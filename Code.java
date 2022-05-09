c61e0ecf9fc06d68a78f90bbeb0bf0dd76759cba
Dmitry Savkin <dimsaz@mail.ru>
2022-05-09 15:18:14 +0200

    fixed

--- Code.java
+++ Code.java
@@ -7,12 +7,13 @@ public class Code {
 
     public static void main(String[] args) {
         try {
-            File f = new File("text.txt");
-            FileWriter fw = new FileWriter(f);
+        
             int a[] = {32, 12, -89, 203, 2};
             for (int i = 0; i <= 6; i++) {
                 System.out.println(a[i]);
             }
+            File f = new File("text.txt");
+            FileWriter fw = new FileWriter(f);
         } catch (IOException e) {
             e.printStackTrace();
         }
