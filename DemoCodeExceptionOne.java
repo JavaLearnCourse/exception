7c986243499db762faa62fdcb7e3d171ddacf673
Dmitry Savkin <dimsaz@mail.ru>
2022-05-09 14:58:38 +0200

    fixed

--- DemoCodeExceptionOne.java
+++ DemoCodeExceptionOne.java
@@ -2,11 +2,17 @@
 public class DemoCodeExceptionOne {
 
 
-    public static double div(int a, int b) {
-        return a * 1. / b;
+    public static double div(int a[], int b) {
+        double sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] / b;
+        }
+        return sum;
     }
+
     public static void main(String[] args) {
-        System.out.println(div(5, 2));
-        System.out.println(div(5, 0));
+        int a [] = {3, 4, 6, -1};
+        System.out.println(div(a, 2));
+        System.out.println(div(a, 0));
     }
 }
